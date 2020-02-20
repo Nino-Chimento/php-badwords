cdaa5274ad9d814a7799b4801975e73a0127beee
Nino Chimento <saninoc84@gmail.com>
2020-02-20 15:48:15 +0100

    sostituzione con get

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $text = " non capisco perche funziona cosi male MAMP";
-$parola = "capisco";
+$parola = $_GET["parola"];
 $nuovo_testo = str_replace($parola, "***", $text)
  ?>
 <!DOCTYPE html>
@@ -13,5 +13,6 @@ $nuovo_testo = str_replace($parola, "***", $text)
     <h1><?php echo $text; ?></h1>
     <h2> <?php echo strlen($text) ?></h2>
     <h3> <?php echo $nuovo_testo; ?></h3>
+    <h3>nuova lunghezza <?php echo strlen($nuovo_testo) ?></h3>
   </body>
 </html>
