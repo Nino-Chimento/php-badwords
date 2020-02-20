aec636f77e0150660a914f05bb7b51d9a36997b1
Nino Chimento <saninoc84@gmail.com>
2020-02-20 14:48:20 +0100

    sostituzione numero con testo

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php echo "string"; ?>
+<?php
+$text = " non capisco perche funziona cosi male MAMP";
+$text = $_GET ["numero"]
+ ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1><?php echo $text ?></h1>
+  </body>
+</html>
